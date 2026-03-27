c596657d519c5a6d58acab88427b0fa31339997b
ThinhLeader <levanthinh32.thptllqt>
2026-03-27 22:39:26 +0700

    update lib mongodb

--- app/api/gallery/route.ts
+++ app/api/gallery/route.ts
@@ -1,6 +1,7 @@
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
+export const runtime = "nodejs"; 
 
 export const revalidate = 0; 
 export async function GET() {
