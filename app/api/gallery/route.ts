9f997033ccd9a311b679086ee15468aac31706fb
ThinhLeader <levanthinh32.thptllqt>
2026-03-27 22:01:59 +0700

    update route

--- app/api/gallery/route.ts
+++ app/api/gallery/route.ts
@@ -1,62 +1,27 @@
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
-import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME!,
-  api_key: process.env.CLOUD_KEY!,
-  api_secret: process.env.CLOUD_SECRET!,
-});
+// GET
 export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db("car-showroom");
 
-    const gallery = await db.collection("galleries").findOne({});
+    const gallery = await db.collection("galleries").findOne();
 
     return NextResponse.json({
       images: gallery?.images || [],
     });
   } catch (err) {
-    console.error("GET GALLERY ERROR:", err);
-
-    return NextResponse.json(
-      { images: [] },
-      { status: 200 }
-    );
-  }
-}
-export async function POST(req: Request) {
-  try {
-    const { file } = await req.json();
-
-    if (!file) {
-      return NextResponse.json(
-        { error: "No file" },
-        { status: 400 }
-      );
-    }
-
-    const result = await cloudinary.uploader.upload(file, {
-      folder: "cars",
-    });
-
-    return NextResponse.json({
-      url: result.secure_url,
-    });
-  } catch (err) {
-    console.error("UPLOAD ERROR:", err);
-    return NextResponse.json(
-      { error: "Upload fail" },
-      { status: 500 }
-    );
+    console.error(err);
+    return NextResponse.json({ images: [] });
   }
 }
 
-// UPDATE
+// PUT
 export async function PUT(req: Request) {
   const body = await req.json();
-    console.log("BODY:", body);
+
   const client = await clientPromise;
   const db = client.db("car-showroom");
 
